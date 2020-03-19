da5a3db6efd0cd2eea827502a1e0459353e31fc4
Ismaël Bussière <45214944+red5h4d0w@users.noreply.github.com>
2020-03-19 18:53:44 -0400

    removed packs from system.json for testing purposes

--- module/templates.js
+++ module/templates.js
@@ -23,3 +23,62 @@ export const preloadHandlebarsTemplates = async function() {
   // Load the template parts
   return loadTemplates(templatePaths);
 };
+
+/*
+    {
+      "name": "classes",
+      "label": "Classes (SRD)",
+      "system": "dnd35e",
+      "path": "./packs/classes.db",
+      "entity": "Item"
+    },
+    {
+      "name": "classfeatures",
+      "label": "Class Features (SRD)",
+      "system": "dnd35e",
+      "path": "./packs/classfeatures.db",
+      "entity": "Item"
+    },
+    {
+      "name": "feats",
+      "label": "Feats (SRD)",
+      "system": "dnd35e",
+      "path": "./packs/feats.db",
+      "entity": "Item"
+    },
+    {
+      "name": "items",
+      "label": "Items (SRD)",
+      "system": "dnd35e",
+      "path": "./packs/items.db",
+      "entity": "Item"
+    },
+    {
+      "name": "monsters",
+      "label": "Monsters (SRD)",
+      "system": "dnd35e",
+      "path": "./packs/monsters.db",
+      "entity": "Actor"
+    },
+    {
+      "name": "prestigeclasses",
+      "label": "Prestige Classes (SRD)",
+      "system": "dnd35e",
+      "path": "./packs/prestigeclasses.db",
+      "entity": "Item"
+    },
+    {
+      "name": "races",
+      "label": "Races (SRD)",
+      "system": "dnd35e",
+      "path": "./packs/races.db",
+      "entity": "Item"
+    },
+    {
+      "name": "spells",
+      "label": "Spells (SRD)",
+      "system": "dnd35e",
+      "path": "./packs/spells.db",
+      "entity": "Item"
+    }
+*/
