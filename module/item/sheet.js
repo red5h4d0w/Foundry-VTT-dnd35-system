5195c6be8a5658589a2820e17384a63b2aa9f3dc
Ismaël Bussière <45214944+red5h4d0w@users.noreply.github.com>
2020-03-27 17:34:11 -0400

    Update sheet.js

--- module/item/sheet.js
+++ module/item/sheet.js
@@ -60,7 +60,7 @@ export class ItemSheet35e extends ItemSheet {
     data.hasAttackRoll = this.item.hasAttack;
     data.isHealing = data.item.data.actionType === "heal";
     data.isFlatDC = getProperty(data.item.data, "save.scaling") === "flat";
-    data.hasCustomRange = this.item.data.data.range.type === "custom";
+    data.hasCustomRange = data.item.data.range.type === "custom";
     return data;
   }
 
